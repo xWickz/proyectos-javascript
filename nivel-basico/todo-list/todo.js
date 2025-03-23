bc9ec371b00e4e00be14a60b06f2acab79f224b0
xWickz <wickzcs@gmail.com>
2025-03-23 19:49:15 -0400

    Code Improved to Look Pretty

--- nivel-basico/todo-list/todo.js
+++ nivel-basico/todo-list/todo.js
@@ -1,37 +1,36 @@
-const input = document.getElementById('tarea');
-const lista = document.getElementById('tareas');
-const boton = document.getElementById('agregar');
-const mensajeImportante = document.getElementById('mensajeImportante');
+const input = document.getElementById("tarea");
+const lista = document.getElementById("tareas");
+const boton = document.getElementById("agregar");
+const mensajeImportante = document.getElementById("mensajeImportante");
 
-boton.addEventListener("click", function() {
-    
-    const container = document.getElementById("tareas");
-    const entrada = input.value.trim();
-    if(!entrada) {
-        mensajeImportante.style.display = "block";
-        mensajeImportante.innerText = "¡Agrega una tarea!";
-        setTimeout(() => {
-            mensajeImportante.innerText = "";
-            mensajeImportante.style.display = "none";
-        }, 2000);
-        return;
-    }
+boton.addEventListener("click", function () {
+  const container = document.getElementById("tareas");
+  const entrada = input.value.trim();
+  if (!entrada) {
+    mensajeImportante.style.display = "block";
+    mensajeImportante.innerText = "¡Agrega una tarea!";
+    setTimeout(() => {
+      mensajeImportante.innerText = "";
+      mensajeImportante.style.display = "none";
+    }, 2000);
+    return;
+  }
 
-    const li = document.createElement('li');
-    const texto = document.createTextNode(entrada);
+  const li = document.createElement("li");
+  const texto = document.createTextNode(entrada);
 
-    const borrar = document.createElement('button');
-    borrar.textContent = 'Borrar';
-    borrar.classList.add('buttontodo');
+  const borrar = document.createElement("button");
+  borrar.textContent = "Borrar";
+  borrar.classList.add("buttontodo");
 
-    borrar.addEventListener('click', function() {
-        li.remove();
-    });
+  borrar.addEventListener("click", function () {
+    li.remove();
+  });
 
-    li.appendChild(texto);
-    li.appendChild(borrar);
+  li.appendChild(texto);
+  li.appendChild(borrar);
 
-    container.appendChild(li);
+  container.appendChild(li);
 
-    input.value = "";
+  input.value = "";
 });
