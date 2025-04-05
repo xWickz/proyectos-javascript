e9b4f5f37e313264eae2467cb4bf65d5d716719a
xWickz <wickzcs@gmail.com>
2025-04-05 11:52:42 -0400

    Changed styles

--- nivel-basico/lista-de-tareas/todo.js
+++ nivel-basico/lista-de-tareas/todo.js
@@ -1,7 +1,8 @@
 const input = document.getElementById("tarea");
 const lista = document.getElementById("tareas");
 const boton = document.getElementById("agregar");
-const mensajeImportante = document.getElementById("mensajeImportante");
+const notify = document.getElementById("notifier");
+let contador = 1;
 
 boton.addEventListener("click", function () {
   const container = document.getElementById("tareas");
@@ -31,21 +32,48 @@ function mostrarMensajeError(mensaje) {
   }, 2000);
 }
 
-  const li = document.createElement("li");
-  const texto = document.createTextNode(entrada);
+  const columna = document.createElement("div");
+  columna.classList.add("column", "is-3");
+
+  const title = document.createElement("p");
+  const titleText = document.createTextNode("Tarea " + contador);
+  title.appendChild(titleText);
+
+  const articulo = document.createElement("article");
+  articulo.classList.add("message");
+
+  const div = document.createElement("div");
+  div.classList.add("message-header");
 
   const borrar = document.createElement("button");
-  borrar.textContent = "Borrar";
-  borrar.classList.add("buttontodo");
+  borrar.classList.add("delete");
+  borrar.setAttribute("arial-label", "Delete");
 
   borrar.addEventListener("click", function () {
-    li.remove();
+    columna.remove();
+    contador--
+
+    if(contador === 1) {
+        notify.textContent = "¡No hay nada! Aún... 😅";
+    }
   });
 
-  li.appendChild(texto);
-  li.appendChild(borrar);
+  div.appendChild(title);
+  div.appendChild(borrar);
+  
 
-  container.appendChild(li);
+  const texto = document.createTextNode(entrada);
+  const text = document.createElement("div");
+  text.classList.add("message-body");
+  text.appendChild(texto);
 
+  articulo.appendChild(div);
+  articulo.appendChild(text);
+
+  columna.appendChild(articulo);
+
+  container.appendChild(columna);
   input.value = "";
+  notify.textContent = "";
+  contador++
 });
