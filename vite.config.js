a673e2549da9b55b864ac3d11071ebe32b3ab55d
xWickz <wickzcs@gmail.com>
2025-04-18 17:13:51 -0400

    Removed useless line

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import taildwindcss from '@tailwindcss/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
