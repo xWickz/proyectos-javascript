5b8e2970dffadff22988acf38e044aa000a38343
xWickz <wickzcs@gmail.com>
2025-04-26 14:23:29 -0400

    Update Pomodoro 2

--- src/components/07_pomodoroTimer.vue
+++ src/components/07_pomodoroTimer.vue
@@ -2,19 +2,28 @@
     [ POMODORO TIMER - 07 ]
     Lista que haceres:
     1. Temporizador
-        * 25 minutos el inicial
-        * 5 minutos el descanso
-        * 15 minutos el descanso largo
+        * 25 minutos el inicial. LISTO
+        * 5 minutos el descanso. LISTO
+        * 15 minutos el descanso largo. LISTO
     2. UI
-        * diseño limpio
-        * botón de comenzar y pausar
+        * diseño limpio. LISTO
+        * botón de comenzar y pausar. LISTO
         * botón de skip
     * que permita enviar notificaciones y sonidos
-    por ahora eso y luego vamos
+    por ahora eso y luego vamos. LISTO
+
+    * Contador de Ciclos
+    * localStorage para guardar: estado actual, ciclo completado
+    * Modo oscuro/claro
+    * historial de pomodoros
+    * tiempo ajustable
+    * nombre del proyecto o tarea
+    
 
 -->
 <template>
-    <section :class="['bg-white dark:text-white h-screen p-5', tipoActual == 'pomodoro' ? 'dark:bg-slate-800 transition-colors duration-400 ease-in-out' : tipoActual == 'shortBreak' ? 'dark:bg-zinc-800 transition-colors duration-400 ease-in-out' : 'dark:bg-stone-800 transition-colors duration-400 ease-in-out']">
+    <section
+        :class="['bg-white dark:text-white h-screen p-5', tipoActual == 'pomodoro' ? 'dark:bg-slate-800 transition-colors duration-400 ease-in-out' : tipoActual == 'shortBreak' ? 'dark:bg-zinc-800 transition-colors duration-400 ease-in-out' : 'dark:bg-stone-800 transition-colors duration-400 ease-in-out']">
 
         <div class="font-bold text-4xl mb-3">
             <mark class="px-2 text-white bg-blue-700 rounded-sm dark:bg-blue-700">
@@ -33,19 +42,31 @@
             <div
                 :class="['mt-15 backdrop-blur-xl w-md block border border-gray-200 rounded-lg shadow-sm p-6 ', tipoActual == 'pomodoro' ? 'dark:bg-gray-700 dark:border-gray-600 transition-colors duration-400 ease-in-out' :tipoActual == 'shortBreak' ? 'dark:bg-gray-600 dark:border-gray-500 transition-colors duration-400 ease-in-out' : 'dark:bg-gray-500 dark:border-gray-400 transition-colors duration-400 ease-in-out']">
                 <div class="grid grid-cols-3">
-                    <button type="button"
-                        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        @click="pomodoroSwitch('pomodoro')">
+                    <button type="button" @click="pomodoroSwitch('pomodoro')" :class="[
+                        'py-2.5 px-5 me-2 mb-2 text-sm font-medium rounded-lg border focus:outline-none',
+                        'focus:z-10 focus:ring-2',
+                        tipoActual === 'pomodoro'
+                            ? 'bg-blue-700 text-white border-blue-700 ring-blue-200 dark:bg-blue-600 dark:border-blue-600 dark:text-white'
+                            : 'text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
+                    ]">
                         Pomodoro
                     </button>
-                    <button type="button"
-                        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        @click="pomodoroSwitch('shortBreak')">
+                    <button type="button" @click="pomodoroSwitch('shortBreak')" :class="[
+                        'py-2.5 px-5 me-2 mb-2 text-sm font-medium rounded-lg border focus:outline-none',
+                        'focus:z-10 focus:ring-2',
+                        tipoActual === 'shortBreak'
+                            ? 'bg-blue-700 text-white border-blue-700 ring-blue-200 dark:bg-blue-600 dark:border-blue-600 dark:text-white'
+                            : 'text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
+                    ]">
                         Short Break
                     </button>
-                    <button type="button"
-                        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        @click="pomodoroSwitch('longBreak')">
+                    <button type="button" @click="pomodoroSwitch('longBreak')" :class="[
+                        'py-2.5 px-5 me-2 mb-2 text-sm font-medium rounded-lg border focus:outline-none',
+                        'focus:z-10 focus:ring-2',
+                        tipoActual === 'longBreak'
+                            ? 'bg-blue-700 text-white border-blue-700 ring-blue-200 dark:bg-blue-600 dark:border-blue-600 dark:text-white'
+                            : 'text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
+                    ]">
                         Long Break
                     </button>
 
@@ -57,28 +78,29 @@
 
                 <div class="justify-center flex mt-8">
                     <button type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                        @click="buttonStatus === 'Comenzar' ? iniciarPomodoro() : detenerPomodoro()">
+                        :class="['text-white focus:ring-2 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  focus:outline-none', buttonStatus === 'Comenzar' ? 'bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-blue-300 dark:focus:ring-blue-800' : 'bg-red-600 hover:bg-red-700 dark:bg-red-500 dark:hover:bg-red-600 focus:ring-red-200 dark:focus:ring-red-700']"
+                        @click="manejarPomodoro">
                         {{ buttonStatus }}
                     </button>
                 </div>
             </div>
-
-            <div></div>
         </div>
+
+        <Footer />
     </section>
 </template>
 
 <script setup>
-import proyectos from '../data/proyectos.js';
 import { ref, onMounted, computed } from 'vue';
+import proyectos from '../data/proyectos.js';
+import Footer from './Footer.vue';
 
+// Definimos variables, el tiempo actual, estado del botón (Comenzar, Pausar) y el pomodoro actual
 let tiempoActual = ref(0);
+let buttonStatus = ref('');
 const tipoActual = ref('');
 
-let buttonStatus = ref('');
-
-// IA AYUDA
+// Relacionado a todo lo del tiempo
 const intervalo = ref(null); // Guardamos el ID del Intervalo. Nos sirve para pararlo.
 const corriendo = ref(false); // Para saber si está activo o pausado.
 
@@ -88,6 +110,10 @@ const segundos = computed(() => tiempoActual.value % 60);
 const formatoTiempo = computed(() => {
     return `${minutos.value}:${segundos.value.toString().padStart(2, '0')}`;
 });
+
+// con los ciclos completados evaluamos cuando merece un descanso corto o largo
+// en este caso son 4 pomodoros para 1 descanso largo :D
+let ciclosCompletados = ref(1);
 
 // Al inicializar la página, en automático se pone en el primer estado.
 onMounted(() => {
@@ -103,8 +129,14 @@ onMounted(() => {
 
     // Cambiamos el valor del tipoActual a pomodoro, sirve para la función iniciarPomodoro().
     tipoActual.value = 'pomodoro';
+
+    // Si aún no ha activado las notificaciones
+    if (Notification.permission !== 'granted') {
+        Notification.requestPermission();
+    }
 });
 
+// Cambiamos el estado del Pomodoro
 function pomodoroSwitch(type) {
     
     tipoActual.value = type;
@@ -112,7 +144,7 @@ function pomodoroSwitch(type) {
     if(type === 'pomodoro') {
         tiempoActual.value = 1500 // 1500 segundos = 25 minutos
     } else if(type === 'shortBreak') {
-        tiempoActual.value = 300 // 300 segundos = 5 minutos
+        tiempoActual.value = 10 // 300 segundos = 5 minutos
     } else {
         tiempoActual.value = 900 // 900 segundos = 15 minutos
     }
@@ -121,6 +153,7 @@ function pomodoroSwitch(type) {
 
 }
 
+// Iniciamos el Pomodoro
 function iniciarPomodoro() {
 
     if(corriendo.value) return; // Evitamos que se inicie varias veces.
@@ -132,7 +165,7 @@ function iniciarPomodoro() {
             tiempoActual.value--;
         } else {
             detenerPomodoro(); // Se detiene cuando llegue a 0 segundos
-            // Aquí va la lógica
+            manejarEstado(); // Cambio de estado automático
         }
     }, 1000);
 
@@ -140,9 +173,50 @@ function iniciarPomodoro() {
 
 }
 
+// Detenemos el intervalo (contador)
 function detenerPomodoro() {
     clearInterval(intervalo.value);
     corriendo.value = false;
     buttonStatus.value = "Comenzar"
 }
+
+// Manejamos el estado del botón, en una función para que sea más limpio
+function manejarPomodoro() {
+    if (buttonStatus.value === 'Comenzar') {
+        iniciarPomodoro();
+    } else {
+        detenerPomodoro();
+    }
+}
+
+function manejarEstado() {
+    if (tipoActual.value === 'pomodoro') {
+        ciclosCompletados.value++;
+        if (ciclosCompletados.value % 4 === 0) {
+            pomodoroSwitch('longBreak');
+            new Notification("Pomodoro Timer", { body: '¡Terminaste 4 ciclos! Ahora toca un descanso largo.' });
+            iniciarPomodoro();
+        } else {
+            pomodoroSwitch('shortBreak');
+            new Notification("Pomodoro Timer", { body: '¡Buen trabajo! Ahora un descanso corto.' });
+            iniciarPomodoro();
+        }
+
+    } else if (tipoActual.value === 'shortBreak') {
+        pomodoroSwitch('pomodoro');
+        new Notification("Pomodoro Timer", { body: 'Descanso corto terminado. ¡Vamos con otro Pomodoro!' });
+        iniciarPomodoro();
+    } else if (tipoActual.value === 'longBreak') {
+        pomodoroSwitch('pomodoro');
+        new Notification("Pomodoro Timer", { body: 'Descanso largo terminado. ¡Vamos a volver con el enfoque al Pomodoro!' });
+        iniciarPomodoro();
+    }
+
+    console.log('CICLOS COMPLETADOS: ', ciclosCompletados.value);
+}
+
+// Solicitamos permiso para enviar notificaciones
+Notification.requestPermission().then((result) => {
+  console.log(result);
+});
 </script>
